3a22dfe3c61e43025880c1a9f33935324a77ae88
Wilfred Hughes <me@wilfred.me.uk>
2018-10-16 00:54:39 +0100

    Make author regex non-greedy

--- monky.el
+++ monky.el
@@ -2257,7 +2257,7 @@ PROPERTIES is the arguments for the function `propertize'."
    "<tags>\\(.?*\\)</tags>"             ; 4. tags
    "<bookmarks>\\(.?*\\)</bookmarks>"   ; 5. bookmarks
    "<phase>\\(.?*\\)</phase>"           ; 6. phase
-   "<author>\\(.?*\\)</author>"         ; 7. author
+   "<author>\\(.*?\\)</author>"         ; 7. author
    "<monky-date>\\([0-9]+\\).?*</monky-date>" ; 8. date
    "\\(.*\\)$"                          ; 9. msg
    ))
